a069c35c274210ffbcb17e603130934a9e7bfb3b
anangyoga <aharwinto@gmail.com>
2022-11-28 22:40:50 +0700

    first commit: hello

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+const myName = "Saski";
+</script>
+
+<template>
+  <div class="flex justify-center bg-yellow-300 items-center h-screen">
+    <div class="text-4xl">Hello 👋🏼</div>
+  </div>
+</template>
